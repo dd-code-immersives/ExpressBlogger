cb50da7c0ba1d63ed0a3cd9bb824b06aadd9f190
Brianna Hickerson <76510136+aloofy92@users.noreply.github.com>
2023-02-06 02:05:48 -0800

    Update users.js

--- validation/users.js
+++ validation/users.js
@@ -1,92 +1,112 @@
 
-// Note: If we have a function, we want to return similar values for all possible return cases.
-const validateUserData = (userData) => {
+const email = req.body.email;
+const firstName = req.body.firstName;
+const lastName = req.body.lastName;
+const age = req.body.age;
+const favoriteFoods = req.body.favoriteFoods;
 
-	/* // EXAMPLE of .filter()
-	const array = ["string", 2, true, "other string"]
 
-	const filteredArray = array.filter((arrayItem)=>{
-		// .filter() will loop through an array we provide and will only return items that match true for the condition that we define
+const userInfo = {
+    email,
+    firstName,
+    lastName,
+    age,
+    favoriteFoods,
+    fullName: firstName + lastName,
+    createdAt: new Date(),
+    lastModified: new Date(),
+};
 
-		// For .filter() the return value must be a boolean and should be true for items that we want to keep
-		if (typeof(arrayItem) === 'string') {
-			return true;
-		} else {
-			return false;
+
+let userInfoValidation = (userInfo) => {
+
+	if (userInfo.firstName === undefined || typeof(userInfo.firstName) !== "string") {
+		return {
+			isValid: false,
+			message: "First name needs to be inputed as a string"
+		}
+	}
+
+	if (userInfo.lastName === undefined || typeof(userInfo.lastName) !== "string") {
+			isValid: false,
+			message; "Last name needs to be inputed as a string"
+		}
+	}
+    if (userInfo.email === undefined || typeof(userInfo.email) !== "string") {
+		return {
+			isValid: false,
+			message: "Email needs to be inputed as a string"
 		}
 
+	if (userInfo.age !== undefined && typeof(userInfo.age) !== "number") {
+		return {
+			isValid: false,
+			message: "Age needs to be inputed as a number"
+		}
+	}
+
+
+
+
+
+
+
+
+
+	let isValid = true;
+	
+	userInfo.favoriteFoods.forEach(food => {
+		
+		if (typeof(food) !== "string" ) {
+				isValid = false;
+			}
 	})
 
-	console.log("array ", array)
-	console.log("filteredArray ", filteredArray) */
-
-	if (userData.email === undefined || typeof(userData.email) !== "string") {
-		// email is required and it must be a string
+	if (isValid === false){
+            
+		return {
+		isValid: false,
+		message: "Food needs to be inputed as a string"
+		}
+	  }
+	
+	if (userInfo.favoriteFoods === undefined || !Array.isArray(userInfo.favoriteFoods) || userInfo.favoriteFoods.length === 0 ) {
 		return {
 			isValid: false,
-			message: "Email is required and it must be a string"
+			message: "favorite foods should be inputed as an array of strings"
 		}
 	}
 
-	if (userData.firstName === undefined || typeof(userData.firstName) !== "string") {
-		// firstName is required and it must be a string
-		return {
-			isValid: false,
-			message: "First name is required and it must be a string"
-		}
-	}
+	
 
-	if (userData.lastName === undefined || typeof(userData.lastName) !== "string") {
-		// lastName is required and it must be a string
-		return {
-			isValid: false,
-			message: "Last name is required and it must be a string"
-		}
-	}
 
-	if (userData.age !== undefined && typeof(userData.age) !== "number") {
-		// age is NOT required, so first we check to see if it even exists before checking to see if the type is anything except 'number'
-		return {
-			isValid: false,
-			message: "Age must be a number"
-		}
-	}
 
-	// Since all array's in JS are objects behind the scenes, typeof(array) will return 'object'. So in order to verify that the variable is actually an array, we have to do the Array.isArray() check.
-	if (userData.favoriteFoods === undefined || !Array.isArray(userData.favoriteFoods) || userData.favoriteFoods.length === 0) {
-		return {
-			isValid: false,
-			message: "favorite foods must be an array and must have length"
-		}
-	}
-
-	// We are going to use .filter() to iterate through favoriteFoods and will only bring through values that are NOT strings. Then we will check if the resultant array has any length, in which case we know there are non-string values in the array.
-	const nonStringFoods = userData.favoriteFoods.filter((favoriteFood)=>{
-
-		// If the callback function in .filter() returns true, then the item will be kept in the resultant array. If the callback returns false, the item will be filtered out
+	let stringFoods = userInfo.favoriteFoods.filter((favoriteFood)=>{
 		if (typeof(favoriteFood) !== 'string') {
 			return true
 		} else {
 			return false
 		}
 	})
+	console.log("stringFoods ", stringFoods)
 
-	console.log("nonStringFoods ", nonStringFoods)
-
-	if (nonStringFoods.length > 0) {
+	if (stringFoods.length > 0) {
 		return {
 			isValid: false,
-			message: "favorite foods must be an array of strings"
+			message: "favorite foods should be inputed as an array of strings"
 		}
 	}
-
 	return {
 		isValid: true
 	}
-}
+
+
+
+
+
+
+
 
 module.exports = {
-	// These two lines are equivalent because the variable name and the key for the object are the same name. Typically, we write the short-hand version.
-	// validateUserData: validateUserData
-	validateUserData,
-}
+	userInfoValidation,
+}}
